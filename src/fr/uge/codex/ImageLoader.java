c232d686246d5735b818b77887afa34aae946836
Epsi <125155335+EpsiDEV@users.noreply.github.com>
2024-06-01 23:03:02 +0200

    [+] gold card draw
    possibilité de dessiner des GoldCard avec l'argument (optionnel) boolean isGolden

--- src/fr/uge/codex/ImageLoader.java
+++ src/fr/uge/codex/ImageLoader.java
@@ -6,33 +6,40 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 import javax.imageio.ImageIO;
 
 import fr.uge.codex.card.CornerType;
 
-/**
- * The ImageLoader class deals with retrieving and storing images from files.
- * 
- * @author vincent
- *
- */
 public class ImageLoader {
-    public static BufferedImage get(CornerType resource, boolean isCard) {
-        if (isCard) {
+	
+	public static BufferedImage get(CornerType resource, boolean isCard) {
+		return get(resource, isCard, false);
+	}
+	
+    public static BufferedImage get(CornerType resource, boolean isCard, boolean isGolden) {
+        Objects.requireNonNull(resource);
+    	
+    	if (isCard) {
+        	if (isGolden) {
+        		return imageFromPath(
+            		Paths.get("./data/img/cards/golden", "golden_" + resource.toString().toLowerCase() + ".png")
+        		); // golden card
+        	}
             return imageFromPath(
-        		"./data/img/cards/" + resource.toString().toLowerCase() + ".png"
-    		);
+        		Paths.get("./data/img/cards", resource.toString().toLowerCase() + ".png")
+    		); // normal card
         }
         
         return imageFromPath(
-        	"./data/img/icons/icon_ " + resource.toString().toLowerCase() + ".png"
-        );
+        	Paths.get("./data/img/icons", "icon_" + resource.toString().toLowerCase() + ".png")
+        ); // icon
     }
 
-    public static BufferedImage imageFromPath(String path) {
+    public static BufferedImage imageFromPath(Path path) {
         try {
-	        return ImageIO.read(Paths.get(path).toFile());
+	        return ImageIO.read(path.toFile());
 	    } catch (IOException e) {
 	        throw new RuntimeException(e);
 	    }
