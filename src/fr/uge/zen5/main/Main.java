fd1cfbb9f09c252e038207b01fa91fca08d5eb06
Epsi <125155335+EpsiDEV@users.noreply.github.com>
2024-06-03 13:56:20 +0200

    Remove synthax error

--- src/fr/uge/zen5/main/Main.java
+++ src/fr/uge/zen5/main/Main.java
@@ -1,11 +1,7 @@
 package fr.uge.zen5.main;
 
-import fr.uge.codex.ResourceCard;
-
 public class Main {
-	
 	public static void main(String[] args) {
-		
-		var card = new ResourceCard(null, null, 0);
+		System.out.println("Main");
 	}
 }
